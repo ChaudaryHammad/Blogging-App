1aa82b758593ea131a1a3180593bfbdc27003f9f
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-01-22 21:27:52 +0500

    Nodejs setup done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.listen(PORT,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(`Server is running on port ${PORT}`);
+
+})
